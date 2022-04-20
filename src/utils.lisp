9f6cd5c9c1234bedbc2c4f444e7bfb4e983c8782
hxz <herongxian@163.com>
2022-04-20 14:43:42 +0800

    documentation for each util

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,5 +1,8 @@
-(in-package :mcas)
+;;;; This file contains the utils used by mcas,
+;;;; and they are originated from Lisp-Actors/useful-macros,
+;;;; with necessary compatility modification.
 
+(in-package :mcas)
 
 ;; parse-body.lisp
 (defun parse-body (body &key documentation whole)
@@ -7,6 +10,9 @@
 Documentation strings are recognized only if DOCUMENTATION is true.
 Syntax errors in body are signalled and WHOLE is used in the signal
 arguments when given."
+  ;; this function can be found in alexandria
+  ;; if documentation is true, parse the string item in body as a docstring, mostly one string allowed.
+  ;; returns: (values function-body-form function-declare-form function-docstring)
   (let ((doc nil)
         (decls nil)
         (current nil))
@@ -16,7 +22,7 @@ arguments when given."
        (when (and documentation (stringp current) (cdr body))
          (if doc
              (error "Too many documentation strings in ~S." (or whole body))
-             (push (pop body) doc)) ;; Alexandira fails here...
+             (push (pop body) doc)) ;; Alexandria fails here...
          (go :declarations))
        (when (and (listp current) (eql (first current) 'declare))
          (push (pop body) decls)
@@ -29,7 +35,8 @@ arguments when given."
        (string= "_" (string x))))
 
 ;; xlambda.lisp
-(defun decl-us (args)
+(defun decl-us (args) ; declare underscore
+  "If args is an underscore symbol, return list ((DECLARE (IGNORE _))) to ignore this arg."
   (when (is-underscore? args)
     `((declare (ignore ,args)))))
 
@@ -39,27 +46,36 @@ arguments when given."
         ((symbolp args) `(&rest ,args))
         (t              args)))
 
-(defun dotted-list-p (lst) ; by hxz, 20211229
+(defun dotted-list-p (lst)
+  "LW doc: The function dotted-list-p is a predicate which tests whether
+list (which must be a cons) is a list ending in a non-nil cdr.
+It returns true if this is the case, otherwise it returns false."
+  ;; in lw (lwpe7.1 win64), calling dotted-list-p with any non-list object will return the object itself.
   (if (listp lst)
       (cdr (last lst))
       lst))
 
+;; xlambda.lisp
 (defun is-lambda-list-keyword (arg)
+  "Test if ARG is a legal lambda list keyword such as &key, &optional, etc."
   (member arg lambda-list-keywords))
 
 ;; xlambda.lisp
 (defun destr-lambda-list-p (args)
+  "Test if a lambda list can be destructured or not."
   (and (consp args)
        (or (eq (car args) '&whole)
            ;;(lw:dotted-list-p args)
-           (dotted-list-p args) ; by hxz, 20211229
+           (dotted-list-p args)
            (some 'consp (subseq args 0
-                                (position-if #'is-lambda-list-keyword args))
-                 ))))
+                                (position-if #'is-lambda-list-keyword args))))))
 
 ;; xlambda.lisp
 (defun wrap-assembly (name args &rest body)
-  (if (destr-lambda-list-p args)
+  "Eg. if NAME is lambda, this function will return a form like (lambda (&rest gensym) new-body),
+the NEW-BODY is a destructuring-bind form which destructures a gensym into ARGS and wraps BODY."
+  ;; this functions a form, and it's purpose is to wrap some definition at some place.
+  (if (destr-lambda-list-p args) ; test if args can be destructured or not
       (let ((g!args (gensym)))
         (multiple-value-bind (body-forms decls docstr)
             (parse-body body :documentation t)
@@ -74,17 +90,22 @@ arguments when given."
 
 ;; xlambda.lisp
 (defmacro lambda* (args &body body)
+  "Return an anonymous function definition, (lambda (&rest gensym) new-body)
+which destructures a gensym and wrap BODY in a destructuring-bind form."
+  ;; this macro's purpose is to wrap a lambda definition at some place.
   (apply #'wrap-assembly 'lambda args body))
 
 ;; useful-macros.lisp
 (defmethod group ((seq sequence) n &key from-end)
+  "Partition SEQ and return a list of SEQUENCE type objects with length N,
+the original order will be kept."
+  ;; (group #(1 2 3 4 5 6 7 8 9 0) 3) -> '(#(1 2 3) #(4 5 6) #(7 8 9) #(0))
   (check-type n (integer 1))
   (let ((stop (length seq)))
     (labels ((rec (start end acc)
                (if (>= end stop)
                    (nreverse (cons (subseq seq start stop) acc))
-                   (rec end (+ end n) (cons (subseq seq start end) acc))
-                   )))
+                   (rec end (+ end n) (cons (subseq seq start end) acc)))))
       (when (plusp stop)
         (cond (from-end
                (let ((start (mod stop n)))
@@ -92,14 +113,12 @@ arguments when given."
                          (< stop n))
                      (rec 0 n nil)
                      (cons (subseq seq 0 start)
-                           (rec start (+ start n) nil))
-                     )))
+                           (rec start (+ start n) nil)))))
+              (t (rec 0 n nil)))))))
 
-              (t
-               (rec 0 n nil))
-              )))))
-
+;; useful-macros.lisp
 (defmethod group ((lst list) n &key from-end)
+  ;; (group '(1 2 3 4 5 6 7 8 9 0) 3) -> '((1 2 3) (4 5 6) (7 8 9) (0))
   (check-type n (integer 1))
   (labels ((rec (source acc)
              (let ((rest (nthcdr n source)))
@@ -115,14 +134,11 @@ arguments when given."
                  ((zerop start) (rec lst nil))
                  (t
                   (cons (subseq lst 0 start)
-                        (rec (nthcdr start lst) nil)))
-                 )))
-
-            (t
-             (rec lst nil))
-            ))))
-
+                        (rec (nthcdr start lst) nil))))))
+            (t (rec lst nil))))))
 
 ;; useful-macros.lisp
 (defun triples (&rest args)
-    (group args 3))
+  "Partition ARGS into a list of lists which length is 3."
+  ;; (triples 1 2 3 4 5 6 7 8 9 0) -> '((1 2 3) (4 5 6) (7 8 9) (0))
+  (group args 3))
